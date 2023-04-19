ec8d1d35949522278fbb100938b40d213b3fddf6
Lancelot <mk148a@hotmail.com>
2023-04-19 14:42:56 +0300

    first init

---
+++ src/Nop.Plugin.Misc.NopWebApi/Infrastructure/ViewLocationExpander.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Nop.Plugin.Misc.NopWebApi.Infrastructure
+{
+    public class ViewLocationExpander : IViewLocationExpander
+    {
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context.AreaName == "Admin")
+            {
+                viewLocations = new[] { $"/Plugins/Nop.Plugin.Misc.NopWebApi/Areas/Admin/Views/{context.ControllerName}/{context.ViewName}.cshtml" }.Concat(viewLocations);
+            }
+            else
+            {
+                viewLocations = new[] { $"/Plugins/Nop.Plugin.Misc.NopWebApi/Views/{context.ControllerName}/{context.ViewName}.cshtml" }.Concat(viewLocations);
+            }
+
+            return viewLocations;
+        }
+    }
+}
