ff92c7c6e93bc3e4d5131c14abf4a4ea5076b2af
Murat <info@hoodarcheryshop.com>
2023-12-08 15:43:36 +0300

    First init

---
+++ src/Nop.Plugin.Misc.ProductCatalogCreator/Migrations/SchemaMigration.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using Nop.Data.Migrations;
+
+namespace Nop.Plugin.Misc.ProductCatalogCreator.Migrations
+{
+    [NopMigration("", "Nop.Plugin.Misc.ProductCatalogCreator schema", MigrationProcessType.Installation)]
+    public class SchemaMigration : AutoReversingMigration
+    {
+        private readonly IMigrationManager _migrationManager;
+
+        public SchemaMigration(IMigrationManager migrationManager)
+        {
+            _migrationManager = migrationManager;
+        }
+
+        /// <summary>
+        /// Collect the UP migration expressions
+        /// </summary>
+        public override void Up()
+        {
+        }
+    }
+}
