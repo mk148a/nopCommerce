080120eebefabf9273ef0c40354bea29f9f08ec4
Lancelot <mk148a@hotmail.com>
2024-02-23 23:36:43 +0300

    First Init

---
+++ src/Plugins/Nop.Plugin.Misc.GoogleShoppingMultiCountry/Components/CustomViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Misc.GoogleShoppingMultiCountry.Components
+{
+    [ViewComponent(Name = "Custom")]
+    public class CustomViewComponent : NopViewComponent
+    {
+        public CustomViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Misc.GoogleShoppingMultiCountry/Nop.Plugin.Misc.GoogleShoppingMultiCountryPlugin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nop.Services.Cms;
+using Nop.Services.Common;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Misc.GoogleShoppingMultiCountry
+{
+    /// <summary>
+    /// Rename this file and change to the correct type
+    /// </summary>
+    public class GoogleShoppingMultiCountry : BasePlugin, IMiscPlugin
+    {
+        public GoogleShoppingMultiCountry()
+        {
+
+        }
+        // <summary>
+        /// Install plugin
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public override async Task InstallAsync()
+        {
+
+            try
+            {
+               
+
+                await base.InstallAsync();
+               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+        }
+
+        /// <summary>
+        /// Uninstall plugin
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+        /// <summary>
+        /// Gets a configuration page URL
+        /// </summary>
+        public override string GetConfigurationPageUrl()
+        {
+            //return _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext).RouteUrl(AccessiBeDefaults.ConfigurationRouteName);
+            //return $"{_webHelper.GetStoreLocation()}Admin/PaymentIyzico/Configure";
+            return "";
+        }
+
+    }
+}
