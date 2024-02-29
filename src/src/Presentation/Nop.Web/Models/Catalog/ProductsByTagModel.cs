86f26a8fd4ffe9683d9f8099d9f3a0e3a516e524
Lancelot <mk148a@hotmail.com>
2024-03-01 00:34:25 +0300

    git fix

---
+++ src/src/Presentation/Nop.Web/Models/Catalog/ProductsByTagModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Web.Models.Catalog
+{
+    public partial record ProductsByTagModel : BaseNopEntityModel
+    {
+        public ProductsByTagModel()
+        {
+            CatalogProductsModel = new CatalogProductsModel();
+        }
+
+        public string TagName { get; set; }
+        public string TagSeName { get; set; }
+        //product tag seo update by Lancelot
+        public string MetaKeywords { get; set; }
+        public string MetaDescription { get; set; }
+        public CatalogProductsModel CatalogProductsModel { get; set; }
+    }
+}
