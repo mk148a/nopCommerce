de92b5c3b5d477a7cbc32e00b00f4582cb523e55
Lancelot <mk148a@hotmail.com>
2024-03-28 18:53:27 +0300

    First Init

---
+++ src/Plugins/Nop.Plugin.Misc.NopTranslator/NopTranslatorSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Misc.NopTranslator
+{
+  
+    public class NopTranslatorSettings : ISettings
+    {
+
+        public string data { get; set; }
+        public bool license { get; set; }
+        public string WidgetZone { get; set; }
+        public int MaximumFile { get; set; }
+        public int MaximumSize { get; set; }
+    }
+}
