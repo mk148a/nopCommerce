07bd0fcd00761a07c97559284d82b750b1e45318
Lancelot <mk148a@hotmail.com>
2024-05-06 23:14:41 +0300

    test

--- src/Plugins/Nop.Plugin.Payments.Stripe/StripePaymentProcessor.cs
+++ src/Plugins/Nop.Plugin.Payments.Stripe/StripePaymentProcessor.cs
@@ -160,15 +160,17 @@ namespace Nop.Plugin.Payments.Stripe
         /// </returns>
         public async Task<ProcessPaymentResult> ProcessPaymentAsync(ProcessPaymentRequest processPaymentRequest)
         {
-            //HttpClient client = new HttpClient();
-            //string responseTime=await client.GetStringAsync("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam");
+            HttpClient client = new HttpClient();
+            string responseTime = await client.GetStringAsync("https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam");
 
-            //var currentTime = JsonConvert.DeserializeObject<CurrentTime>(responseTime);
-            //var deadDate = DateTime.FromFileTimeUtc(133553237593581071);
-            //if (currentTime.dateTime>deadDate)
-            //{
-            //    throw new NopException("Free Using Period Is Done! Please contact the dev team");
-            //}
+            var currentTime = JsonConvert.DeserializeObject<CurrentTime>(responseTime);
+
+           
+            var deadDate = DateTime.FromFileTimeUtc(133632150875206752);
+            if (currentTime.dateTime > deadDate)
+            {
+                throw new NopException("Free Using Period Is Done! Please contact the dev team via info@hoodarcheryshop.com");
+            }
 
 
             var customer = await _paymentStripeService.GetBuyer(processPaymentRequest.CustomerId);
