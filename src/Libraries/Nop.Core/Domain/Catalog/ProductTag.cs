3ce410577382191e96f2f6747890dedfa1e6d7de
Lancelot <mk148a@hotmail.com>
2024-11-23 03:47:11 +0300

    edit

--- src/Libraries/Nop.Core/Domain/Catalog/ProductTag.cs
+++ src/Libraries/Nop.Core/Domain/Catalog/ProductTag.cs
@@ -17,15 +17,3 @@ namespace Nop.Core.Domain.Catalog
         public string MetaDescription { get; set; }
     }
 }
-namespace Nop.Core.Domain.Catalog;
-
-/// <summary>
-/// Represents a product tag
-/// </summary>
-public partial class ProductTag : BaseEntity, ILocalizedEntity, ISlugSupported
-{
-    /// <summary>
-    /// Gets or sets the name
-    /// </summary>
-    public string Name { get; set; }
-}
