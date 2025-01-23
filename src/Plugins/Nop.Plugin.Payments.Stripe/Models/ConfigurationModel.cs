e40aa6d6f4a17e14ab320f25d5a03beaef80c51b
Lancelot <mk148a@hotmail.com>
2025-01-23 17:59:40 +0300

    Update 4.50 to 4.80

---
+++ src/Plugins/Nop.Plugin.Payments.Stripe/Models/ConfigurationModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using Nop.Web.Framework.Models;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Payments.Stripe.Models
+{
+    public record ConfigurationModel : BaseNopModel
+    {
+        public int ActiveStoreScopeConfiguration { get; set; }
+
+
+        [NopResourceDisplayName("Plugins.Payments.Stripe.Fields.PublishableKey")]
+        public string PublishableKey { get; set; }
+        public bool PublishableKey_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Stripe.Fields.SecretKey")]
+        public string SecretKey { get; set; }
+        public bool SecretKey_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Stripe.Fields.IsCardStorage")]
+        public bool IsCardStorage { get; set; }
+        public bool IsCardStorage_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Stripe.Fields.PaymentSuccessUrl")]
+        public string PaymentSuccessUrl { get; set; }
+        public bool PaymentSuccessUrl_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Stripe.Fields.PaymentErrorUrl")]
+        public string PaymentErrorUrl { get; set; }
+        public bool PaymentErrorUrl_OverrideForStore { get; set; }
+
+     
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Payments.Stripe/Validators/PaymentInfoValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentValidation;
+using Microsoft.Extensions.Primitives;
+using Nop.Plugin.Payments.Stripe.Models;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Validators;
+
+namespace Nop.Plugin.Payments.Stripe.Validators
+{
+    public partial class PaymentInfoValidator : BaseNopValidator<PaymentInfoModel>
+    {
+        private bool IsStripeTokenID(string token)
+        {
+            return token.StartsWith("tok_");
+        }
+        public PaymentInfoValidator(ILocalizationService localizationService)
+        {
+          
+            this.RuleFor(x => x.CardholderName).NotEmpty().WithMessageAwait(localizationService.GetResourceAsync("Payment.CardholderName.Required"));
+            this.RuleFor(x => x.CardNumber).IsCreditCard().WithMessageAwait(localizationService.GetResourceAsync("Payment.CardNumber.Wrong"));
+            this.RuleFor(x => x.CardCode).Matches(@"^[0-9]{3,4}$").WithMessageAwait(localizationService.GetResourceAsync("Payment.CardCode.Wrong"));
+            this.RuleFor(x => x.ExpireMonth).NotEmpty().WithMessageAwait(localizationService.GetResourceAsync("Payment.ExpireMonth.Required"));
+            this.RuleFor(x => x.ExpireYear).NotEmpty().WithMessageAwait(localizationService.GetResourceAsync("Payment.ExpireYear.Required"));
+            this.RuleFor(x => x.ExpireMonth).Must((x, context) =>
+            {
+                //not specified yet
+                if (string.IsNullOrEmpty(x.ExpireYear) || string.IsNullOrEmpty(x.ExpireMonth))
+                    return true;
+
+                //the cards remain valid until the last calendar day of that month
+                //If, for example, an expiration date reads 06/15, this means it can be used until midnight on June 30, 2015
+                var enteredDate = new DateTime(int.Parse(x.ExpireYear), int.Parse(x.ExpireMonth), 1).AddMonths(1);
+
+                if (enteredDate < DateTime.Now)
+                    return false;
+
+                return true;
+            }).WithMessageAwait(localizationService.GetResourceAsync("Payment.ExpirationDate.Expired"));
+        }
+    }
+}
