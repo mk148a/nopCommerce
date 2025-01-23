fd718c903d3aa6d32b6d3f07740c9568136fe1f3
Lancelot <mk148a@hotmail.com>
2025-01-23 17:59:40 +0300

    Update 4.50 to 4.80

---
+++ src/Plugins/Nop.Plugin.Payments.Stripe/Mapping/Builders/PluginBuilder.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Data.Mapping.Builders;
+using Nop.Plugin.Payments.Stripe.Domains;
+
+namespace Nop.Plugin.Payments.Stripe.Mapping.Builders
+{
+    public class PluginBuilder : NopEntityBuilder<CustomTable>
+    {
+        #region Methods
+
+        public override void MapEntity(CreateTableExpressionBuilder table)
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Payments.Stripe/StripePaymentDefaults.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Payments.Stripe
+{
+    public static class StripePaymentDefaults
+    {
+        /// <summary>
+        /// Stripe payment method system name
+        /// </summary>
+        public static string SystemName => "Payments.Stripe";
+
+        /// <summary>
+        /// Name of the view component to display plugin in public store
+        /// </summary>
+        public const string ViewComponentName = "PaymentStripe";
+
+        /// <summary>
+        /// User agent used for requesting Stripe services
+        /// </summary>
+        public static string UserAgent => "Stripe-connect-nopCommerce-4.5-1.0";
+
+        /// <summary>
+        /// Path to the Stripe payment form js script
+        /// </summary>
+        public static string PaymentFormScriptPath => "https://js.stripe.com/v3/";
+
+
+        /// <summary>
+        /// Note passed for each payment transaction
+        /// </summary>
+        /// <remarks>
+        /// {0} : Order Guid
+        /// </remarks>
+        public static string PaymentNote => "nopCommerce Order Id: {0}";
+    }
+}
