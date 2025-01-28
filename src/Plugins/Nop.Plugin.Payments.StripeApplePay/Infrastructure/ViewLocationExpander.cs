c08b786f7cf50c04fa2116d86d6961d7107f1231
Lancelot <mk148a@hotmail.com>
2025-01-28 17:20:50 +0300

    FİRST İNİT FROM 4.50

---
+++ src/Plugins/Nop.Plugin.Payments.StripeApplePay/Infrastructure/ViewLocationExpander.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Nop.Plugin.Payments.StripeApplePay.Infrastructure
+{
+    public class ViewLocationExpander : IViewLocationExpander
+    {
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context.AreaName == "Admin")
+            {
+                viewLocations = new[] { $"/Plugins/Nop.Plugin.Payments.StripeApplePay/Areas/Admin/Views/{context.ControllerName}/{context.ViewName}.cshtml" }.Concat(viewLocations);
+            }
+            else
+            {
+                viewLocations = new[] { $"/Plugins/Nop.Plugin.Payments.StripeApplePay/Views/{context.ControllerName}/{context.ViewName}.cshtml" }.Concat(viewLocations);
+            }
+
+            return viewLocations;
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Payments.StripeApplePay/Services/IPaymentStripeApplePayService.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Threading.Tasks;
+using Nop.Plugin.Payments.StripeApplePay.Models;
+
+namespace Nop.Plugin.Payments.StripeApplePay.Services
+{
+    public interface IPaymentStripeApplePayService
+    {
+        Task<Buyer> GetBuyer(int customerId);
+    }
+}
