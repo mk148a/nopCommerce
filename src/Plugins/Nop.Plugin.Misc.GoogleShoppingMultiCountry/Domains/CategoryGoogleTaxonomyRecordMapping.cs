2b7cace836ba2d0176702f959c86282541941d8f
Lancelot <mk148a@hotmail.com>
2025-01-31 02:09:20 +0300

    Refactoring 4.50 to 4.80

---
+++ src/Plugins/Nop.Plugin.Misc.GoogleShoppingMultiCountry/Domains/CategoryGoogleTaxonomyRecordMapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Core;
+
+namespace Nop.Plugin.Misc.GoogleShoppingMultiCountry.Domains
+{
+    public partial class CategoryGoogleTaxonomyRecordMapping : BaseEntity
+    {
+        public int GoogleTaxonomyRecordId { get; set; }
+        public int CategoryId { get; set; }
+       
+
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Misc.GoogleShoppingMultiCountry/Models/GoogleFeedProductModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.Misc.GoogleShoppingMultiCountry.Models
+{
+    public record GoogleFeedProductModel : BaseNopEntityModel
+    {
+        #region Ctor
+
+        public GoogleFeedProductModel()
+        {
+            GoogleFeedProductListSearchModel = new GoogleFeedProductSearchModel();
+        }
+
+        #endregion
+
+        public int ProductId { get; set; }
+
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.Products.ProductName")]
+        public string ProductName { get; set; }
+
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.Products.GoogleCategory")]
+        public string GoogleCategory { get; set; }
+
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.Products.Gender")]
+        public string Gender { get; set; }
+
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.Products.AgeGroup")]
+        public string AgeGroup { get; set; }
+
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.Products.Color")]
+        public string Color { get; set; }
+
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.Products.Size")]
+        public string GoogleSize { get; set; }
+
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.CustomGoods")]
+        public bool CustomGoods { get; set; }
+        [NopResourceDisplayName("Plugins.Misc.GoogleShoppingMultiCountry.Products.LanguageId")]
+        public int LanguageId{ get; set; }
+        public GoogleFeedProductSearchModel GoogleFeedProductListSearchModel { get; set; }
+    }
+}
